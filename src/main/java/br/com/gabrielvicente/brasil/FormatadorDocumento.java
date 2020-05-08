d49869f4d0d4c2a6540d71b43e9fdf1bd3e3a8aa
gabrielvf64 <gabrielvf64>
2020-05-08 09:19:28 -0300

    formatar cnpj e titulo de eleitor

--- src/main/java/br/com/gabrielvicente/brasil/FormatadorDocumento.java
+++ src/main/java/br/com/gabrielvicente/brasil/FormatadorDocumento.java
@@ -1,12 +1,14 @@
 package br.com.gabrielvicente.brasil;
 
+import br.com.caelum.stella.format.CNPJFormatter;
 import br.com.caelum.stella.format.CPFFormatter;
+import br.com.caelum.stella.format.TituloEleitoralFormatter;
 
 public class FormatadorDocumento {
     public static void main(String[] args) {
         String cpf = "862.883.667-57";
         String cnpj = "10.156.788/0001-83";
-        String tituloDeEleitor = "815520840108";
+        String tituloEleitor = "815520840108";
 
         CPFFormatter formatadorCPF = new CPFFormatter();
 
@@ -14,5 +16,26 @@ public class FormatadorDocumento {
 
         System.out.println(cpf);
         System.out.println(cpfSemFormatacao);
+
+        imprimeLinhaSeparadora();
+
+        CNPJFormatter formatadorCNPJ = new CNPJFormatter();
+
+        String cnpjSemFormatacao = formatadorCNPJ.unformat(cnpj);
+
+        System.out.println(cnpj);
+        System.out.println(cnpjSemFormatacao);
+
+        imprimeLinhaSeparadora();
+
+        TituloEleitoralFormatter formatadorTitulo = new TituloEleitoralFormatter();
+        String tituloComFormatacao = formatadorTitulo.format(tituloEleitor);
+
+        System.out.println(tituloEleitor);
+        System.out.println(tituloComFormatacao);
+    }
+
+    private static void imprimeLinhaSeparadora() {
+        System.out.println("======================");
     }
 }
