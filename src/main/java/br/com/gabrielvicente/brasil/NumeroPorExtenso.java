e690dc4e76d0d59ac1526b51446ca860975e0469
gabrielvf64 <gabrielvf64>
2020-05-09 14:46:54 -0300

    usar a FormatoDeReal para escrever reais e a parte dos centavos

--- src/main/java/br/com/gabrielvicente/brasil/NumeroPorExtenso.java
+++ src/main/java/br/com/gabrielvicente/brasil/NumeroPorExtenso.java
@@ -1,5 +1,6 @@
 package br.com.gabrielvicente.brasil;
 
+import br.com.caelum.stella.inwords.FormatoDeReal;
 import br.com.caelum.stella.inwords.InteiroSemFormato;
 import br.com.caelum.stella.inwords.NumericToWordsConverter;
 
@@ -11,5 +12,10 @@ public class NumeroPorExtenso {
         BigDecimal valor = new BigDecimal("75.00");
         String valorPorExtenso = conversor.toWords(valor.doubleValue());
         System.out.println(valorPorExtenso + " reais");
+
+        NumericToWordsConverter conversorFormatoReais = new NumericToWordsConverter(new FormatoDeReal());
+        BigDecimal valor2 = new BigDecimal("75.10");
+        String valorPorExtenso2 = conversorFormatoReais.toWords(valor2.doubleValue());
+        System.out.println(valorPorExtenso2);
     }
 }
