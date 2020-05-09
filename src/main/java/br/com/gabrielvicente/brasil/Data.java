289d96e6a743af354183dfbb658e7da075123e35
gabrielvf64 <gabrielvf64>
2020-05-09 12:59:33 -0300

    usar LocalDateTime para data e hora

--- src/main/java/br/com/gabrielvicente/brasil/Data.java
+++ src/main/java/br/com/gabrielvicente/brasil/Data.java
@@ -1,13 +1,23 @@
 package br.com.gabrielvicente.brasil;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Data {
     public static void main(String[] args) {
         LocalDate hoje = LocalDate.now();
         System.out.println(hoje);
+        System.out.println();
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         System.out.println(hoje.format(formatador));
+
+        System.out.println();
+
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora);
+        System.out.println();
+        DateTimeFormatter formatadorComHora = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+        System.out.println(agora.format(formatadorComHora));
     }
 }
